48f30236d9b1bed915c571244e3e134ee8ba6391
Kennedy Njeri <mistakenz123@gmail.com>
2019-12-25 12:14:31 +0300

    Added palindrone and permutation

---
+++ palindrone.py
@@ -0,0 +1,24 @@
+"""Give a string, write a function to determine if it is a palindrone"""
+
+# racecar
+# madam
+# Dammit I'm mad.
+
+import string
+
+
+def is_palindrone(s):
+    s = s.lower()
+    s = s.translate(None, string.punctuation)
+    s = s.replace(" ", "")
+
+    return s == s[::-1]
+
+
+str_1 = "racecar"
+str_2 = "Dammit I'm mad"
+str_3 = "computer"
+
+print (is_palindrone(str_1))
+print (is_palindrone(str_2))
+print (is_palindrone(str_3))
---
+++ unique.py
@@ -0,0 +1,12 @@
+"""Implement an algorithm to determine if a string has all unique characters"""
+
+
+def is_unique(s):
+    return len(s) == len(set(s))
+
+
+s1 = "unique"
+s2 = "bear"
+
+print (is_unique(s1))
+print (is_unique(s2))
